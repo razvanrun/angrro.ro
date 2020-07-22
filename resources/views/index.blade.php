a4d049431ab80745a0c0cd1ac54f71be6c168fa0
Razvan <razvanrun@localhost.localdomain>
2020-07-22 09:52:20 +0300

    angrro.v.0.9.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,46 +12,107 @@ Angroo.ro - Cumpara si Vinde Angro pe Angrro.ro
     <section id="BrowseCategories">
         <div class="container">
             <h2 class="h2 text-center">Cauta Anunturi</h2>
-            <h4 class="h4 text-center mb-5 text-muted">Gaseste Inspiratia pentru Afacerea sau Familia ta</h4>
+            <h4 class="h4 text-center mb-5 text-muted">Gaseste Inspiratia pentru Afacerea si Familia ta</h4>
             <div class="row mb-5">
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=1">
-                   <img  class="icon-category"  src="/storage/assets/handshake-icon-flat-style_169241-482.jpg" alt="">
+                   <img  class="icon-category"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
                    <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=2">
-                  <img  class="icon-category"  src="/storage/assets/images (3).png" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/L6Chsfh/images-3.png" alt="">
                   <h5 class="h5 text-muted">Animale de Companie</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=3">
-                  <img  class="icon-category"  src="/storage/assets/baby-twin-flat-icon-vector-4961010.jpg" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/1TRsLS0/baby-twin-flat-icon-vector-4961010.jpg" alt="">
                     <h5 class="h5 text-muted">Nou Nascuti</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=4">
-                  <img  class="icon-category"  src="/storage/assets/truck_farm_agriculture_agronomy-512.png" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
                     <h5 class="h5 text-muted"> Agronomie </h5></a>
                 </div>
             </div>
             <div class="row mb-5">
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=5">
-                  <img  class="icon-category"  src="/storage/assets/images (2).png" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/6RqnT18/images-2.png" alt="">
                    <h5 class="h5 text-muted">Imobiliare</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=6">
-                  <img  class="icon-category"  src="/storage/assets/casa.jpg" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
                     <h5 class="h5 text-muted">Casa si Gradina</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=7">
-                  <img  class="icon-category"  src="/storage/assets/829497_beauty_512x512.png" alt="">
+                  <img  class="icon-category"  src="https://i.ibb.co/8PGhvyk/829497-beauty-512x512.png" alt="">
                     <h5 class="h5 text-muted">Cosmetice</h5></a>
                 </div>
                 <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=8">
-                  <img class="icon-category" src="/storage/assets/images (1).png" alt="">
+                  <img class="icon-category" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
                     <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
                 </div>
             </div>
         </div>
     </section>
+    <!-- Mobile Categories Start -->
+    <div class="container-mobile-category">
+
+      <div class="content-wrapper">
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=1">
+             <img  class="icon-category-mobile"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
+             <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=2">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/L6Chsfh/images-3.png" alt="">
+            <h5 class="h5 text-muted">Animale de Companie</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=3">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/1TRsLS0/baby-twin-flat-icon-vector-4961010.jpg" alt="">
+              <h5 class="h5 text-muted">Nou Nascuti</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=4">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
+              <h5 class="h5 text-muted"> Agronomie </h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=5">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/6RqnT18/images-2.png" alt="">
+             <h5 class="h5 text-muted">Imobiliare</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=6">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
+              <h5 class="h5 text-muted">Casa si Gradina</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=7">
+            <img  class="icon-category-mobile"  src="https://i.ibb.co/8PGhvyk/829497-beauty-512x512.png" alt="">
+              <h5 class="h5 text-muted">Cosmetice</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=8">
+            <img class="icon-category-mobile" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
+              <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
+          </div>
+        </div>
+        <div class="child">
+          <div class="text-center"><a href="/userdashboard?cat=9">
+            <img class="icon-category-mobile" src="https://i.ibb.co/6HGgnrR/1803330.png" alt="">
+              <h5 class="h5 text-muted">Locuri de Munca</h5></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Mobile Categories Stop -->
 
 
-    
+
 @endsection
