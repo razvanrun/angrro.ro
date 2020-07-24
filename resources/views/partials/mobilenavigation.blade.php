ae09b221c91b16981c3896a635b273e66a4d1874
Razvan <razvanrun@localhost.localdomain>
2020-07-25 01:33:45 +0300

    angrro.v.0.9.7.5

---
+++ resources/views/partials/mobilenavigation.blade.php
@@ -0,0 +1,61 @@
+<!-- Mobile Categories Start -->
+<div class="container-mobile-category">
+
+  <div class="content-wrapper">
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=1">
+         <img  class="icon-category-mobile"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
+         <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=2">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/L6Chsfh/images-3.png" alt="">
+        <h5 class="h5 text-muted">Animale de Companie</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=3">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/TcPw0SP/baby-twin-flat-icon-vector-4961010.png" alt="">
+          <h5 class="h5 text-muted">Nou Nascuti</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=4">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
+          <h5 class="h5 text-muted"> Agronomie </h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=5">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/6RqnT18/images-2.png" alt="">
+         <h5 class="h5 text-muted">Imobiliare</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=6">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
+          <h5 class="h5 text-muted">Casa si Gradina</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=7">
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/hMg7xD9/beauty.png" alt="">
+          <h5 class="h5 text-muted">Cosmetice</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=8">
+        <img class="icon-category-mobile" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
+          <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
+      </div>
+    </div>
+    <div class="child">
+      <div class="text-center"><a href="/userdashboard?cat=9">
+        <img class="icon-category-mobile" src="https://i.ibb.co/6HGgnrR/1803330.png" alt="">
+          <h5 class="h5 text-muted">Locuri de Munca</h5></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Mobile Categories Stop -->
