e6bc87a0661caaa8d573b00b70d70c978cf68f76
Razvan <razvanrun@localhost.localdomain>
2020-08-10 11:10:34 +0300

    r8

--- resources/views/partials/mobilenavigation.blade.php
+++ resources/views/partials/mobilenavigation.blade.php
@@ -1,29 +1,28 @@
 <!-- Mobile Categories Start -->
 <div class="container-mobile-category">
-
   <div class="content-wrapper">
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=1">
-         <img  class="icon-category-mobile"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
-         <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
+        <img class="icon-category-mobile" src="https://i.ibb.co/6HGgnrR/1803330.png" alt="">
+          <h5 class="h5 text-muted">Locuri de Munca</h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=2">
-        <img  class="icon-category-mobile"  src="https://i.ibb.co/W3dQdkM/central-asian-shepherd-dog-flat-icon-doggy-head-vector-18623218.jpg" alt="">
-        <h5 class="h5 text-muted">Animale de Companie</h5></a>
+        <img class="icon-category-mobile" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
+          <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=3">
-        <img  class="icon-category-mobile"  src="https://i.ibb.co/TcPw0SP/baby-twin-flat-icon-vector-4961010.png" alt="">
-          <h5 class="h5 text-muted">Nou Nascuti</h5></a>
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/hMg7xD9/beauty.png" alt="">
+          <h5 class="h5 text-muted">Cosmetice</h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=4">
-        <img  class="icon-category-mobile"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
-          <h5 class="h5 text-muted"> Agronomie </h5></a>
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
+          <h5 class="h5 text-muted">Casa si Gradina</h5></a>
       </div>
     </div>
     <div class="child">
@@ -34,26 +33,26 @@
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=6">
-        <img  class="icon-category-mobile"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
-          <h5 class="h5 text-muted">Casa si Gradina</h5></a>
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
+          <h5 class="h5 text-muted"> Agronomie </h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=7">
-        <img  class="icon-category-mobile"  src="https://i.ibb.co/hMg7xD9/beauty.png" alt="">
-          <h5 class="h5 text-muted">Cosmetice</h5></a>
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/TcPw0SP/baby-twin-flat-icon-vector-4961010.png" alt="">
+          <h5 class="h5 text-muted">Nou Nascuti</h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=8">
-        <img class="icon-category-mobile" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
-          <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
+        <img  class="icon-category-mobile"  src="https://i.ibb.co/W3dQdkM/central-asian-shepherd-dog-flat-icon-doggy-head-vector-18623218.jpg" alt="">
+        <h5 class="h5 text-muted">Animale de Companie</h5></a>
       </div>
     </div>
     <div class="child">
       <div class="text-center"><a href="/userdashboard?cat=9">
-        <img class="icon-category-mobile" src="https://i.ibb.co/6HGgnrR/1803330.png" alt="">
-          <h5 class="h5 text-muted">Locuri de Munca</h5></a>
+         <img  class="icon-category-mobile"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
+         <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
       </div>
     </div>
   </div>
