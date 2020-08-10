375c8c8bacc413c37def4a5ae80c13dba15f6925
Razvan <razvanrun@localhost.localdomain>
2020-08-10 11:00:43 +0300

    r7r

--- resources/views/partials/desktopnavigation.blade.php
+++ resources/views/partials/desktopnavigation.blade.php
@@ -3,49 +3,6 @@
         <h2 class="h2 text-center">Cauta Anunturi</h2>
         <h4 class="h4 text-center mb-5 text-muted">Gaseste Inspiratia pentru Afacerea si Familia ta</h4>
         <div class="row mb-5">
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=1">
-               <img  class="icon-category"  src="https://i.ibb.co/Bsxz8f9/handshake-icon-flat-style-169241-482.jpg" alt="">
-               <h5 class="h5 text-muted">Servicii & Oferte</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=2">
-              <img  class="icon-category"  src="https://i.ibb.co/W3dQdkM/central-asian-shepherd-dog-flat-icon-doggy-head-vector-18623218.jpg" alt="">
-              <h5 class="h5 text-muted">Animale de Companie</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=3">
-              <img  class="icon-category"  src="https://i.ibb.co/TcPw0SP/baby-twin-flat-icon-vector-4961010.png" alt="">
-                <h5 class="h5 text-muted">Nou Nascuti</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=4">
-              <img  class="icon-category"  src="https://i.ibb.co/Zf8B1RK/truck-farm-agriculture-agronomy-512.png" alt="">
-                <h5 class="h5 text-muted"> Agronomie </h5></a>
-            </div>
-        </div>
-        <div class="row mb-5">
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=5">
-              <img  class="icon-category"  src="https://i.ibb.co/6RqnT18/images-2.png" alt="">
-               <h5 class="h5 text-muted">Imobiliare</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=6">
-              <img  class="icon-category"  src="https://i.ibb.co/QYkyPtb/casa.jpg" alt="">
-                <h5 class="h5 text-muted">Casa si Gradina</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=7">
-              <img  class="icon-category"  src="https://i.ibb.co/hMg7xD9/beauty.png" alt="">
-                <h5 class="h5 text-muted">Cosmetice</h5></a>
-            </div>
-            <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=8">
-              <img class="icon-category" src="https://i.ibb.co/LgCv0Yz/images-1.png" alt="">
-                <h5 class="h5 text-muted">Electronice & Electrocasnice</h5></a>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="BrowseCategories">
-    <div class="container">
-        <h2 class="h2 text-center">Cauta Anunturi</h2>
-        <h4 class="h4 text-center mb-5 text-muted">Gaseste Inspiratia pentru Afacerea si Familia ta</h4>
-        <div class="row mb-5">
           <div class="col-sm text-center hvr-grow"><a href="/userdashboard?cat=1">
             <img class="icon-category" src="https://i.ibb.co/6HGgnrR/1803330.png" alt="">
               <h5 class="h5 text-muted">Locuri de Munca</h5></a>
