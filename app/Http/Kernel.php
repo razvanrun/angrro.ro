d175037962759a293af7a0eaedd8dabd72f9b67d
Razvan <razvanrun@localhost.localdomain>
2020-08-18 05:42:14 +0300

    angrro.v.0.9.9

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,6 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,  
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,11 @@
+<div class="js-cookie-consent cookie-consent">
+
+    <span class="cookie-consent__message">
+      Pentru a iti imbunatatii experienta acest website foloseste cookies
+    </span>
+
+    <button class="js-cookie-consent-agree cookie-consent__agree">
+        Accepta cookies
+    </button>
+
+</div>
