48519008e790c00f46e7ee6e5b62dc2078321d2c
Razvan <razvanrun@localhost.localdomain>
2020-08-23 02:52:31 +0300

    e28

--- resources/views/freelancer/userdashboard.blade.php
+++ resources/views/freelancer/userdashboard.blade.php
@@ -17,14 +17,24 @@
             @include('partials.alert')
             <div class="card card-default">
             	<div class="row">
-<div>
+<!-- <div>
 <div class="search">
-	<!-- <input type="text" name="q" class="searchbox" placeholder="Search Twitter"> -->
-  <input type="text" class="searchbox" placeholder="Search Jobs" aria-label="Amount (rounded to the nearest dollar)" aria-describedby="basic-addon" name="search">
-	<!-- <button type="submit" class="searchbtn"><i class="fas fa-search"></i></button> -->
+  <input type="text" class="searchbox" placeholder="Search Jobs" aria-label="" aria-describedby="basic-addon" name="search">
   <button class="searchbtn " id="basic-addon" type="submit"><i class="fas fa-search"></i></button>
 </div>
-</div>
+</div> -->
+
+<form action="/search" method="POST" role="search">
+    {{ csrf_field() }}
+    <div class="input-group">
+        <input type="text" class="form-control searchbox" name="q"
+            placeholder="Search users"> <span class="input-group-btn">
+            <button type="submit" class="btn btn-default searchbtn">
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+        </span>
+    </div>
+</form>
 
 	                <div class="card-body col-lg-9">
 	                    @if(count($jobs) > 0)
