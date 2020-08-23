97914130b555980b179e734c651192af24983ce4
Razvan <razvanrun@localhost.localdomain>
2020-08-23 03:49:37 +0300

    e34

--- resources/views/freelancer/userdashboard.blade.php
+++ resources/views/freelancer/userdashboard.blade.php
@@ -17,45 +17,13 @@
             @include('partials.alert')
             <div class="card card-default">
             	<div class="row">
-<!-- <div>
+<div>
 <div class="search">
   <input type="text" class="searchbox" placeholder="Search Jobs" aria-label="" aria-describedby="basic-addon" name="search">
   <button class="searchbtn " id="basic-addon" type="submit"><i class="fas fa-search"></i></button>
 </div>
-</div> -->
-
-<form action="{{ url('/search')}}" method="POST" role="search">
-    {{ csrf_field() }}
-    <div class="input-group search">
-        <input type="text" class="form-control searchbox" name="q"
-            placeholder="Search users"> <span class="input-group-btn">
-            <button type="submit" class="btn btn-default searchbtn">
-                <span class="glyphicon glyphicon-search"></span>
-            </button>
-        </span>
-    </div>
-</form>
-@if(isset($details))
-<div class="card-body col-lg-9">
-      @foreach ($details as $job)
-  <div class="product-listing-container">
-<section class="product-listing-image-box" >
-<img class="product-listing-image" src="{{url('public/images',$job->position_type)}}" id="product-image"> </li>
-</section>
-<section class="product-listing-info-box" >
-<ul class="product-listing-details-ul" >
-<li class="product-listing-title-li" >  <p class="product-listing-title-p"><a href="jobs/{{$job->id}}" class="product-listing-title-a">{{$job->title}}</a></p> </li>
-<li class="product-listing-price-li" >  <span class="product-listing-price-span" >{{$job->budget}} LEU/RON</span> </li>
-</ul>
-</section>
 </div>
 
-      @endforeach
-      {{ $jobs->links() }}
-
-</div>
-
-@endif
 	                <div class="card-body col-lg-9">
 	                    @if(count($jobs) > 0)
 		                    @foreach ($jobs as $job)
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Input;
-use App\Job;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -119,16 +116,3 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 // Search Controller
-
-
-Route::post ( '/search', function () {
-    $q = Input::get ('q');
-    if($q != ""){
-        $job = Job::where('title', 'LIKE', '%' . q . '%')
-                      ->orWhere('body', 'LIKE', '%' . q . '%')
-                      ->get();
-        if(count(@job) > 0)
-            return view('userdashboard')->withDetails($job)->withQuery($q);
-    }
-    return view('userdashboard')->withMessage("Nu a fost gasit un anunt!");
-} );
